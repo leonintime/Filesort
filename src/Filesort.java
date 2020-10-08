4fa49655181ae08e36d94b697ca588338688c1ea
ldierk <ldierk@intime.de>
2020-10-08 15:10:16 +0200

    Optimized code

--- src/Filesort.java
+++ src/Filesort.java
@@ -20,69 +20,77 @@ public class Filesort {
         this.fileFolder = fileFolder;
         this.currentPath = currentPath;
         this.destinationPath = destinationPath;
+        this.picAmount = 0;
+        this.wordFileAmount = 0;
+        this.excelFileAmount = 0;
+        this.pdfAmount = 0;
     }
-
 
 
     public void sortFiles() {
         try {
-                File folder = new File(fileFolder);
-                File[] listedFiles = folder.listFiles();
-                for (File file : listedFiles) {
-                    String fileName = file.getName();
-                    // Word Dokumente werden sortiert
-                    if (fileName.contains(".docx")) {
-                        Path temp = Files.move(Paths.get(currentPath + fileName),
-                                Paths.get(destinationPath + fileName));
-                        wordFileAmount++;
-                        if (temp != null) {
-                            System.out.println(fileName + "got successfully moved");
-                        } else {
-                            System.out.println("Failed to move the file");
-                        }
+            File folder = new File(fileFolder);
+            File[] listedFiles = folder.listFiles();
+            for (File file : listedFiles) {
+                String fileName = file.getName();
+                // Word Dokumente werden sortiert
+                if (fileName.contains(".docx")) {
+                    Path temp = Files.move(Paths.get(currentPath + fileName),
+                            Paths.get(destinationPath + fileName));
+                    wordFileAmount++;
+                    if (temp != null) {
+                        System.out.println(fileName + "got successfully moved");
+                    } else {
+                        System.out.println("Failed to move the file");
                     }
-
-                    // Excel Dateien werden sortiert
-                    else if (fileName.contains(".xlsx")) {
-                        Path temp = Files.move(Paths.get(currentPath + fileName),
-                                Paths.get(destinationPath + fileName));
-                        excelFileAmount++;
-                        if (temp != null) {
-                            System.out.println(fileName + "got successfully moved");
-                        } else {
-                            System.out.println("Failed to move the file");
-                        }
-                    }
-
-
-                    // Bild Dateien werden sortiert
-                    else if (fileName.contains(".jpg") || fileName.contains(".jpeg") || fileName.contains(".gif") || fileName.contains(".png")) {
-                        Path temp = Files.move(Paths.get(currentPath + fileName),
-                                Paths.get(destinationPath + fileName));
-                        picAmount++;
-                        if (temp != null) {
-                            System.out.println(fileName + "got successfully moved");
-                        } else {
-                            System.out.println("Failed to move the file");
-                        }
-
-                        // Pdf Dateien werden sortiert
-                    } else if (fileName.contains(".pdf")) {
-                        Path temp = Files.move(Paths.get(currentPath + fileName),
-                                Paths.get(destinationPath + fileName));
-                        pdfAmount++;
-                        if (temp != null) {
-                            System.out.println(fileName + "got successfully moved");
-                        } else {
-                            System.out.println("Failed to move the file");
-                        }
-                    }
-
                 }
+
+                // Excel Dateien werden sortiert
+                else if (fileName.contains(".xlsx")) {
+                    Path temp = Files.move(Paths.get(currentPath + fileName),
+                            Paths.get(destinationPath + fileName));
+                    excelFileAmount++;
+                    if (temp != null) {
+                        System.out.println(fileName + "got successfully moved");
+                    } else {
+                        System.out.println("Failed to move the file");
+                    }
+                }
+
+
+                // Bild Dateien werden sortiert
+                else if (fileName.contains(".jpg") || fileName.contains(".jpeg") || fileName.contains(".gif") || fileName.contains(".png")) {
+                    Path temp = Files.move(Paths.get(currentPath + fileName),
+                            Paths.get(destinationPath + fileName));
+                    picAmount++;
+                    if (temp != null) {
+                        System.out.println(fileName + "got successfully moved");
+                    } else {
+                        System.out.println("Failed to move the file");
+                    }
+
+                    // Pdf Dateien werden sortiert
+                } else if (fileName.contains(".pdf")) {
+                    Path temp = Files.move(Paths.get(currentPath + fileName),
+                            Paths.get(destinationPath + fileName));
+                    pdfAmount++;
+                    if (temp != null) {
+                        System.out.println(fileName + "got successfully moved");
+                    } else {
+                        System.out.println("Failed to move the file");
+                    }
+                }
+
+            }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
+
+
+
+
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,32 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Filesort filesort = new Filesort(System.getenv("Download_folder"), System.getenv("Download_folder"), System.getenv("Word_files"));
-        filesort.sortFiles();
+
+        Filesort sortWordFilesDownload = new Filesort(System.getenv("Download_folder"), System.getenv("Download_folder"), System.getenv("Word_files"));
+        Filesort sortExcelFilesDownload = new Filesort(System.getenv("Download_folder"), System.getenv("Download_folder"), System.getenv("Excel_files"));
+        Filesort sortPdfFilesDownload = new Filesort(System.getenv("Download_folder"), System.getenv("Download_folder"), System.getenv("Pdf_files"));
+        Filesort sortPicturesDownload = new Filesort(System.getenv("Download_folder"), System.getenv("Download_folder"), System.getenv("Pictures"));
+
+
+        Filesort sortWordFilesDesktop = new Filesort(System.getenv("Desktop"), System.getenv("Desktop"), System.getenv("Word_files"));
+        Filesort sortExcelFilesDesktop = new Filesort(System.getenv("Desktop"), System.getenv("Desktop"), System.getenv("Excel_files"));
+        Filesort sortPdfFilesDesktop = new Filesort(System.getenv("Desktop"), System.getenv("Desktop"), System.getenv("Pdf_files"));
+        Filesort sortPicturesDesktop = new Filesort(System.getenv("Desktop"), System.getenv("Desktop"), System.getenv("Pictures"));
+
+
+        // Files in download folder are getting moved to the destination folder
+        sortWordFilesDownload.sortFiles();
+        sortExcelFilesDownload.sortFiles();
+        sortPdfFilesDownload.sortFiles();
+        sortPicturesDownload.sortFiles();
+
+        // Files on the desktop folder are getting moved to the destination folder
+        sortWordFilesDesktop.sortFiles();
+        sortExcelFilesDesktop.sortFiles();
+        sortPdfFilesDesktop.sortFiles();
+        sortPicturesDesktop.sortFiles();
+
+
     }
 
 
