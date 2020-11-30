f20eb00355447413854c72efe0cffc08129417ae
ldierk <ldierk@intime.de>
2020-11-30 16:04:43 +0100

    old change

--- src/com/filesort/org/Database.java
+++ src/com/filesort/org/Database.java
@@ -8,8 +8,8 @@ public class Database {
     private String sql;
     private Statement statement;
 
-    public Database(String conString) throws SQLException {
-        conn = DriverManager.getConnection(conString);
+    public Database(String DB_CON) throws SQLException {
+        conn = DriverManager.getConnection(DB_CON);
         this.statement = conn.createStatement();
     }
 
@@ -21,30 +21,6 @@ public class Database {
         return sql;
     }
 
-    public boolean addPath(String sql) {
-
-        try {
-            statement = conn.createStatement();
-            statement.execute(sql);
-            return true;
-        } catch (SQLException e) {
-            e.getMessage();
-            return false;
-        }
-    }
-
-
-    public void updatePath(String sql) {
-
-        try {
-            statement = conn.createStatement();
-            statement.execute(sql);
-        } catch (SQLException e) {
-            e.getMessage();
-        }
-    }
-
-
     public void showPaths() {
 
         try {
@@ -53,11 +29,43 @@ public class Database {
             while (results.next()) {
                 System.out.println(results.getObject("path_name") + " " + results.getObject("path_value"));
             }
-            results.close();
+            closeCon();
         } catch (SQLException e) {
             e.getMessage();
         }
     }
+
+    public boolean addPath(String pathName, String pathValue) {
+
+        try {
+            String sql = "INSERT INTO paths (path_name, path_value) VALUES ('" + pathName + "','" + pathValue + "')";
+            statement = conn.createStatement();
+            statement.execute(sql);
+            closeCon();
+            return true;
+        } catch (SQLException e) {
+            e.getMessage();
+            return false;
+        }
+    }
+
+
+    public boolean updatePath(String pathName, String pathValue, int pathNumber) {
+
+        try {
+            String sql = "UPDATE paths SET path_name = '" + pathName + "', path_value = '" + pathValue + "' WHERE path_id = + " + pathName + " ";
+            statement = conn.createStatement();
+            statement.execute(sql);
+            closeCon();
+            return true;
+        } catch (SQLException e) {
+            e.getMessage();
+            return false;
+        }
+    }
+
+
+
 
 
     public void closeCon() {
